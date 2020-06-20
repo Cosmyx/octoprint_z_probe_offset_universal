9459af3b8e5790a59366d13ddafd5cab18df05b6
razer <razerraz@free.fr>
2020-06-20 18:50:12 +0200

    Fix bug (#1): Creality3D variant offset catch 2d try

--- octoprint_z_probe_offset/__init__.py
+++ octoprint_z_probe_offset/__init__.py
@@ -91,7 +91,7 @@ class Z_probe_offset_plugin(octoprint.plugin.AssetPlugin,
             self.prusa_firmware = 'prusa' in payload.get('name').lower()
 
     def set_z_offset_from_printer_response(self, offset):
-        offset = offset.replace(' ', '')
+        offset = offset.replace(' ', '').replace('"', '').strip()
         if not offset:
             self._logger.warning('Offset part is empty !')
             return
